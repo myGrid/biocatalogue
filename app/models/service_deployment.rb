fd551d9a731d0229d2c0e44ce3bc2a604f608354
jits <jits@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2008-10-07 22:03:05 +0000

    Updated provider association with appropriate foreign key.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@48 1acaf488-1b9d-4964-8077-0a2fdb83ef00

--- app/models/service_deployment.rb
+++ app/models/service_deployment.rb
@@ -2,7 +2,8 @@ class ServiceDeployment < ActiveRecord::Base
   belongs_to :service
   
   belongs_to :provider, 
-             :class_name => "ServiceProvider"
+             :class_name => "ServiceProvider",
+             :foreign_key => "provider_id"
   
   belongs_to :service_version
 end
