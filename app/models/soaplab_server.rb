58435f8d1f05a87fb2450eafb1fd60830ba280fe
ebontane <ebontane@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2008-10-07 08:27:57 +0000

    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@45 1acaf488-1b9d-4964-8077-0a2fdb83ef00

--- app/models/soaplab_server.rb
+++ app/models/soaplab_server.rb
@@ -1,24 +1,20 @@
+require 'open-uri'
+
 class SoaplabServer < ActiveRecord::Base
-  
-  #has_many :soap_services, :dependent => :destroy
+  before_create :save_services
    
   def save_services
     
     @wsdl_urls = get_wsdl_from_server(self.location)
-    @test_wsdls = @wsdl_urls.first(2)
+    @test_wsdls = @wsdl_urls.first(10)
     @test_wsdls.each { |url|
     
-    puts url 
-    
-    @web_service  = WebService.new
-    @web_service.service_type = 'SOAP'
-    @web_service.save
-    @soap_service = @web_service.soap_services.build
-    @soap_service.name = 'soaplab_service'
-    @soap_service.wsdl_location = url
-    @soap_service.description = url
-    @soap_service.new_service_attributes = @soap_service.get_service_attributes(url)
-    @soap_service.save
+    transaction do
+      @soap_service = SoapService.new
+      @soap_service.wsdl_location = url
+      @soap_service.description = url
+      @soap_service.save(false)
+    end
     } 
   end
     
@@ -26,9 +22,10 @@ class SoaplabServer < ActiveRecord::Base
   # a soaplab server
   def get_wsdl_from_server(server_url)
     wsdls = []  
-    open(server_url).each { |line|
+    open(server_url.strip()).each { |line|
       wsdls << line.split('"')[1] if line =~ /http:/}
   
     wsdls 
   end
+ 
 end
