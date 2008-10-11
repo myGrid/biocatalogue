cd66df0b19ada83ce3f7ca26e1ef31cb4f2e1253
jits <jits@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2008-10-11 21:40:45 +0000

    Added SERVICE_TYPES hash to main biocat config. This maps the different service type supported with the corresponding model class.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@60 1acaf488-1b9d-4964-8077-0a2fdb83ef00

---
+++ config/initializers/biocat.rb
@@ -0,0 +1,6 @@
+# Mappings for service types supported, to corresponding model classes.
+SERVICE_TYPES = {
+  "SOAP Web Service"  => SoapService,
+  "REST Web Service"  => RestService,
+  "Soaplab Server"    => SoaplabServer
+}
