2af41fc5004c7b10da72d0e989eb607464e242fd
tlaurent <tlaurent@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2008-10-14 10:44:40 +0000

    use new layout
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@75 1acaf488-1b9d-4964-8077-0a2fdb83ef00

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -12,6 +12,8 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
+
+  layout "application_wide"
   
   def login_required
     respond_to do |format|
