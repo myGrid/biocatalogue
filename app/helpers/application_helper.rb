6e9b7d53bc8815b1cb023596e160cf8222ce4fa1
jits <jits@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2008-10-31 10:05:02 +0000

    Added flag images and Country Codes handling code (all from myExperiment) to allow us to easily display flags for locations.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@112 1acaf488-1b9d-4964-8077-0a2fdb83ef00

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,29 @@ module ApplicationHelper
     controller_name.humanize.titleize
   end
   
+  def flag_icon(country, text=country, style='')
+    return '' if country.blank?
+    
+    code = ''
+    
+    if country.downcase == "great britain"
+      code = "gb"
+    elsif ["england", "wales", "scotland"].include?(country.downcase)
+      code = country
+    elsif country.length > 2
+      code = CountryCodes.code(country)
+    else
+      code = country if CountryCodes.valid_code?(country)
+    end
+    
+    #puts "code = " + code
+    
+    unless code.blank?
+      return image_tag("flags/#{code.downcase}.png",
+              :title => "header=[] body=[<b>Location: </b>#{text}] cssheader=[boxoverTooltipHeader] cssbody=[boxoverTooltipBody] delay=[200]",
+              :style => "vertical-align:middle; #{style}")
+    else
+      return ''
+    end
+  end
 end
