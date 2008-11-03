600f7225997b9f788af54565b94355fc7a3b6425
jits <jits@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2008-11-03 13:13:52 +0000

    Updated root of site to point to services index page.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@123 1acaf488-1b9d-4964-8077-0a2fdb83ef00

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :services
   
   # Root of website
-  map.root :controller => 'users', :action => 'new'
+  map.root :controller => 'services', :action => 'index'
   
   # The priority is based upon order of creation: first created -> highest priority.
 
