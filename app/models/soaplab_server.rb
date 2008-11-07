6e7cd608e434effbf5c9290fdedc098fd97fc459
ebontane <ebontane@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2008-11-07 10:31:27 +0000

    use the new soap model populate funtion
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@131 1acaf488-1b9d-4964-8077-0a2fdb83ef00

--- app/models/soaplab_server.rb
+++ app/models/soaplab_server.rb
@@ -2,32 +2,61 @@ require 'open-uri'
 
 class SoaplabServer < ActiveRecord::Base
   acts_as_trashable
-  
+  validates_presence_of :name, :location
+  validates_uniqueness_of :location, :message => "already exists in BioCatalogue"
+  validates_url_format_of :location,
+                          :allow_nil => false
   before_create :save_services
+  before_update :save_services
+  #@service_urls = {}
+  attr_accessor :wsdl_urls
+  
+  @wsdl_urls = []
    
   def save_services
     
     @wsdl_urls = get_wsdl_from_server(self.location)
-    @test_wsdls = @wsdl_urls.first(10)
-    @test_wsdls.each { |url|
     
-    transaction do
-      @soap_service = SoapService.new
-      @soap_service.wsdl_location = url
-      @soap_service.description = url
-      @soap_service.save(false)
-    end
-    } 
+    unless @wsdl_urls.empty?  
+      new_wsdls = new_existing_urls(@wsdl_urls)['new'] 
+      
+      new_wsdls.each { |url|
+      transaction do
+        soap_service = SoapService.new
+        soap_service.wsdl_location = url
+        soap_service.populate
+        soap_service.save!
+     
+        end
+      }
+     end
   end
     
   # hack to get the wsdl documents from
   # a soaplab server
   def get_wsdl_from_server(server_url)
-    wsdls = []  
-    open(server_url.strip()).each { |line|
-      wsdls << line.split('"')[1] if line =~ /http:/}
+    begin
+      wsdls = []  
+      open(server_url.strip()).each { |line|
+        wsdls << line.split('"')[1] if line =~ /http:/ or line =~ /https:/}
+      wsdls 
+    rescue
+      errors.add_to_base("there were problems accessing the soaplab server")
+      wsdls
+    end
+  end
   
-    wsdls 
+  def new_existing_urls(urls=[])
+    urls = urls || get_wsdl_from_server(self.location)
+    all_urls     = []
+    @service_urls = {}
+    
+    SoapService.find(:all).each{ |s| all_urls << s.wsdl_location }
+    new = urls - all_urls
+    @service_urls['new']      = new
+    @service_urls['existing'] = urls - new
+    
+    @service_urls
   end
  
 end
