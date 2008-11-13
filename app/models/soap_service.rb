31efcd0492a1385b798d8f624e9f3ca2ea678814
ebontane <ebontane@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2008-11-13 16:36:14 +0000

    moved post create function from controller into model
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@215 1acaf488-1b9d-4964-8077-0a2fdb83ef00

--- app/models/soap_service.rb
+++ app/models/soap_service.rb
@@ -88,6 +88,43 @@ class SoapService < ActiveRecord::Base
     "SOAP"
   end
   
+  def post_create(soap_service, endpoint, current_user)
+    # Try and find location of the service from the url of the WSDL.
+    wsdl_geo_location = BioCatalogue::Util.url_location_lookup(soap_service.wsdl_location)
+    city = (wsdl_geo_location.nil? || wsdl_geo_location.city.blank? || wsdl_geo_location.city == "(Unknown City)") ? nil : wsdl_geo_location.city
+    country = (wsdl_geo_location.nil? || wsdl_geo_location.country_code.blank?) ? nil : CountryCodes.country(wsdl_geo_location.country_code)
+    
+    # Create the associated service, service_version and service_deployment objects.
+    # We can assume here that this is the submission of a completely new service in BioCatalogue.
+    
+    new_service = Service.new(:name => soap_service.name)
+    
+    new_service.submitter = current_user
+                              
+    new_service_version = new_service.service_versions.build(:version => "1", 
+                                                             :version_display_text => "1")
+    
+    new_service_version.service_versionified = soap_service
+    new_service_version.submitter = current_user
+    
+    new_service_deployment = new_service_version.service_deployments.build(:endpoint => endpoint,
+                                                                           :city => city,
+                                                                           :country => country)
+    
+    new_service_deployment.provider = ServiceProvider.find_or_create_by_name(Addressable::URI.parse(soap_service.wsdl_location).host)
+    new_service_deployment.service = new_service
+    new_service_deployment.submitter = current_user
+                                                  
+    if new_service.save
+      return true
+    else
+      logger.error("ERROR: post_create method for SoapServicesController failed!")
+      logger.error("Error messages: #{new_service.errors.full_messages.to_sentence}")
+      return false
+    end
+  end
+ 
+  
 protected
 
   # This builds the parts of the SOAP service 
