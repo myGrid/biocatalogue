94cbdc4f9767cdea2236bc7c0f546dc3a0b0481a
jits <jits@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2008-11-17 15:14:29 +0000

    Changed resolr scripts to now rebuild the search index the proper way!
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@229 1acaf488-1b9d-4964-8077-0a2fdb83ef00

--- resolr_dev.sh
+++ resolr_dev.sh
@@ -2,7 +2,7 @@
 
 export RAILS_ENV=development
 
-echo '(Service.find(:all).map do |s| s.solr_save end) if ENABLE_SEARCH' | ruby script/console
-echo '(User.find(:all).map do |u| u.solr_save end) if ENABLE_SEARCH' | ruby script/console
-echo '(ServiceProvider.find(:all).map do |s| s.solr_save end) if ENABLE_SEARCH' | ruby script/console
+echo '(Service.rebuild_solr_index) if ENABLE_SEARCH' | ruby script/console
+echo '(User.rebuild_solr_index) if ENABLE_SEARCH' | ruby script/console
+echo '(ServiceProvider.rebuild_solr_index) if ENABLE_SEARCH' | ruby script/console
 
