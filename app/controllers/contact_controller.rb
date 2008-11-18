4a6007deee894a5482a34d7ab38ade9cabc844ea
tlaurent <tlaurent@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2008-11-18 11:26:08 +0000

    add index action
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@241 1acaf488-1b9d-4964-8077-0a2fdb83ef00

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,2 +1,14 @@
+# BioCatalogue: app/controllers/contact_controller.rb
+#
+# Copyright (c) 2008, University of Manchester, The European Bioinformatics 
+# Institute (EMBL-EBI) and the University of Southampton.
+# See license.txt for details.
+
 class ContactController < ApplicationController
+  # GET /contact
+  def index
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
 end
