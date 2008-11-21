ce627316e5107ffd4bf4f6408f9af9b3f57c4a6d
ebontane <ebontane@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2008-11-21 10:38:57 +0000

    catch and log exceptions resulting from service submission
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@270 1acaf488-1b9d-4964-8077-0a2fdb83ef00

--- app/models/soaplab_server.rb
+++ app/models/soaplab_server.rb
@@ -9,36 +9,52 @@ require 'open-uri'
 class SoaplabServer < ActiveRecord::Base
   acts_as_trashable
   validates_presence_of :name, :location
-  validates_uniqueness_of :location, :message => "already exists in BioCatalogue"
+  #validates_uniqueness_of :location, :message => "already exists in BioCatalogue"
   validates_url_format_of :location,
                           :allow_nil => false
   #before_create :save_services
-  before_update :save_services
+  #before_update :save_services
   #@service_urls = {}
   attr_accessor :wsdl_urls
   
   @wsdl_urls = []
    
   def save_services(current_user)
-    
-    @wsdl_urls = get_wsdl_from_server(self.location)
+    @error_urls        = []
+    @existing_services = []
+    @new_services      = []
+    @wsdl_urls         = get_wsdl_from_server(self.location)
     
     unless @wsdl_urls.empty?  
-      new_wsdls = new_existing_urls(@wsdl_urls)['new'] 
-      
-      new_wsdls.each { |url|
-      transaction do
-        soap_service = SoapService.new
-        soap_service.wsdl_location = url
-        success, data = soap_service.populate
-        soap_service.save!
-        if soap_service.save
-          soap_service.post_create(soap_service, data["endpoint"], current_user)
+      @wsdl_urls.first(5).each { |url|
+      if SoapService.find(:first, :conditions => ["wsdl_location = ?", url])
+         @soap_service = SoapService.find(:first, :conditions => ["wsdl_location = ?", url])
+         @existing_services << @soap_service.service(true) if @soap_service != nil
+      else
+        #new_wsdls.each { |url|
+        transaction do
+          begin
+            soap_service = SoapService.new({:wsdl_location => url})
+            success, data = soap_service.populate
+            if success and soap_service.save!
+              pc_success = soap_service.post_create(data["endpoint"], current_user) 
+              if pc_success
+                logger.info("INFO: registered service - #{url}. SUCCESS:")
+              else
+                @error_urls << url
+                logger.error("ERROR: post_create failed for service - #{url}. ")
+              end
+            end
+          rescue Exception => ex
+            @error_urls << url
+            logger.error("ERROR: failed to register service - #{url}. soaplab registration Exception:")
+            logger.error(ex)
+          end
         end
-     
-        end
+      end
       }
-     end
+    end
+    return [@new_services, @existing_services, @error_urls]
   end
     
   # hack to get the wsdl documents from
