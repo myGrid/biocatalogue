5b2f994df0aa73a7be8c312d16c45d33589a650f
ebontane <ebontane@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2009-01-28 16:40:10 +0000

    added function to generate listing from a soaplab server
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@391 1acaf488-1b9d-4964-8077-0a2fdb83ef00

--- app/models/soaplab_server.rb
+++ app/models/soaplab_server.rb
@@ -13,7 +13,8 @@ class SoaplabServer < ActiveRecord::Base
   
   acts_as_annotatable
   
-  validates_presence_of :name, :location
+  #validates_presence_of :name, :location
+  validates_presence_of :location
   #validates_uniqueness_of :location, :message => "already exists in BioCatalogue"
   validates_url_format_of :location,
                           :allow_nil => false
@@ -29,7 +30,7 @@ class SoaplabServer < ActiveRecord::Base
     @error_urls        = []
     @existing_services = []
     @new_services      = []
-    @wsdl_urls         = get_wsdls_from_server(self.location)
+    @wsdl_urls         = get_info_from_server()[0]
     
     unless @wsdl_urls.empty?  
       @wsdl_urls.each { |url|
@@ -64,25 +65,35 @@ class SoaplabServer < ActiveRecord::Base
   end
     
   
-  #Get list of wsdls from server by using the soaplab server API
-  def get_wsdls_from_server(server_wsdl)
+  #Get list of wsdls and tools from server by using the soaplab server API
+  def get_info_from_server(server_wsdl=self.location)   
     begin
-      wsdls = [] 
+      result = [] 
       proxy = SOAP::WSDLDriverFactory.new(server_wsdl).create_rpc_driver
-      wsdls = get_wsdls(proxy)
+      result = get_wsdls_and_tool_names(proxy)
     rescue
-      errors.add_to_base("there were problems getting wsdls from server ")  
-      wsdls
+      errors.add_to_base("there were problems getting wsdls and tool names from server ")  
+      result
     end
   end
   
-  def get_wsdls(proxy)
-    wsdls = []
+  def get_wsdls_and_tool_names(proxy)
+    wsdls = [self.location]
     base  = proxy.getServiceLocation("").return
     tools = proxy.getAvailableAnalyses("").return
     tools.each{ |t| wsdls << File.join(base, t+'?wsdl')}
     
-    return wsdls
+    return [wsdls, tools]
   end
+  
+  def find_services_in_catalogue
+    wsdls    = get_info_from_server()[0]
+    services = []
+    wsdls.each{ |url|
+         obj = SoapService.find(:first, :conditions => { :wsdl_location => url })
+         services << (obj.nil? ? nil : obj.service)     
+     }
+     return services.compact
+   end
    
 end
