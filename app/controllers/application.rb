4a23778365a9f5f8add43f376663ac1c597bc3f3
tlaurent <tlaurent@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2009-02-04 14:03:35 +0000

    does not need to set session[:original_uri] as it's always performed in the before_filter
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@409 1acaf488-1b9d-4964-8077-0a2fdb83ef00

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,6 +1,6 @@
 # BioCatalogue: app/controllers/application.rb
 #
-# Copyright (c) 2008, University of Manchester, The European Bioinformatics 
+# Copyright (c) 2008, University of Manchester, The European Bioinformatics
 # Institute (EMBL-EBI) and the University of Southampton.
 # See license.txt for details.
 
@@ -11,30 +11,30 @@ require_dependency RAILS_ROOT + '/vendor/plugins/annotations/lib/app/controllers
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-  
+
   #do not log the plain text passwords
   filter_parameter_logging "password"
-  
+
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery :secret => 'bc5fa0462513829e6a733e8947c24994'
-  
-  # See ActionController::Base for details 
+
+  # See ActionController::Base for details
   # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
+  # from your application log (in this case, all fields with names like "password").
   # filter_parameter_logging :password
 
   layout "application_wide"
-  
+
   before_filter :set_original_uri
-  
+
   def login_required
     respond_to do |format|
       format.html do
         if session[:user_id]
           @current_user = User.find(session[:user_id])
         else
-          session[:original_uri] = request.request_uri
+          #session[:original_uri] = request.request_uri
           flash[:notice] = "Please log in"
           redirect_to new_session_url
         end
@@ -51,13 +51,13 @@ class ApplicationController < ActionController::Base
       end
     end
   end
-  
+
   # Returns true or false if the user is logged in.
   def logged_in?
     return session[:user_id] ? true : false
   end
   helper_method :logged_in?
-  
+
   # Check Administrator status for a user.
   # To make a user an Administrator, edit manually the user
   # in the datatbase: assign 'role_id' to 1
@@ -77,29 +77,29 @@ class ApplicationController < ActionController::Base
   # in the datatbase: assign 'role_id' to 1
   def check_admin?
     unless logged_in? && !current_user.nil? && current_user.role_id == 1
-      session[:original_uri] = request.request_uri
+      #session[:original_uri] = request.request_uri
       flash[:error] = "<b>Action denied.</b><p>This action is restricted to Administrators.</p>"
       redirect_to new_session_url
     end
   end
   helper_method :check_admin?
-  
+
   # Accesses the current user from the session.
   def current_user
     @current_user ||= (session[:user_id] && User.find(session[:user_id])) || nil
   end
   helper_method :current_user
-  
+
   # Check if an object belongs to the user logged in
   def mine?(thing)
     return false if thing.nil?
     return false unless logged_in?
-    
+
     c_id = current_user.id.to_i
-    
+
     case thing.class.to_s
     when "User"
-      return c_id == thing.id 
+      return c_id == thing.id
     when "Annotation"
       return thing.source == current_user
     else
@@ -107,22 +107,22 @@ class ApplicationController < ActionController::Base
     end
   end
   helper_method :mine?
-  
+
   def set_sidebar_layout
     self.class.layout "application_sidebar"
   end
-  
+
   def set_no_layout
     self.class.layout nil
   end
-  
+
   def disable_action
     # This will cause a 404...
     raise ActionController::UnknownAction.new
   end
-  
+
   def set_original_uri
-    unless controller_name.downcase == 'sessions'
+    unless controller_name.downcase == 'sessions' || action_name.downcase == 'activate_account'
       session[:original_uri] = request.request_uri if not logged_in?
     end
   end
