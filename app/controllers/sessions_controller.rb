3dd66528ef348739f7d2cd707d12968e056da2db
tlaurent <tlaurent@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2009-02-04 14:00:02 +0000

    do not use HTTP_REFERER if session[:original_uri] is defined -> fix bug for 'wrong activation code' message
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@407 1acaf488-1b9d-4964-8077-0a2fdb83ef00

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,23 @@
 # BioCatalogue: app/controllers/sessions_controller.rb
 #
-# Copyright (c) 2008, University of Manchester, The European Bioinformatics 
+# Copyright (c) 2008, University of Manchester, The European Bioinformatics
 # Institute (EMBL-EBI) and the University of Southampton.
 # See license.txt for details.
 
 class SessionsController < ApplicationController
 
   def new
-    session[:original_uri] = request.env['HTTP_REFERER'] if not logged_in?
+    if not logged_in?
+      session[:original_uri] = request.env['HTTP_REFERER'] unless session[:original_uri] != nil
+    end
   end
-  
+
   def create
     user = User.authenticate(params[:login], params[:password])
     if user
       session[:user_id] = user.id
       flash[:notice] = "Welcome #{user.display_name} !"
-      
+
       session[:original_uri].blank? ? redirect_to(home_url) : redirect_to(session[:original_uri])
       session[:original_uri] = nil
     else
@@ -24,7 +26,7 @@ class SessionsController < ApplicationController
       render :action => :new
     end
   end
-  
+
   def destroy
     reset_session
     flash[:notice] = "You've been logged out."
