b015d52d50405874de5f471fe7551ee8e4a8d38f
tlaurent <tlaurent@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2009-03-10 17:12:15 +0000

    work on the styles and icons for adding annotations and tags
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@481 1acaf488-1b9d-4964-8077-0a2fdb83ef00

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # BioCatalogue: app/helpers/application_helper.rb
 #
-# Copyright (c) 2008, University of Manchester, The European Bioinformatics 
+# Copyright (c) 2008, University of Manchester, The European Bioinformatics
 # Institute (EMBL-EBI) and the University of Southampton.
 # See license.txt for details.
 
@@ -9,16 +9,16 @@
 require_dependency RAILS_ROOT + '/vendor/plugins/annotations/lib/app/helpers/application_helper'
 
 module ApplicationHelper
-  
+
   def controller_visible_name(controller_name)
     controller_name.humanize.titleize
   end
-  
+
   def flag_icon_from_country(country, text=country, style="margin-left: 0.5em;")
     return '' if country.blank?
-    
+
     code = ''
-    
+
     if country.downcase == "great britain"
       code = "gb"
     elsif ["england", "wales", "scotland"].include?(country.downcase)
@@ -28,16 +28,16 @@ module ApplicationHelper
     else
       code = country if CountryCodes.valid_code?(country)
     end
-    
+
     #puts "code = " + code
-    
+
     unless code.blank?
       return flag_icon_from_country_code(code, text, style)
     else
       return ''
     end
   end
-  
+
   def flag_icon_from_country_code(code, text=nil, style="margin-left: 0.5em;")
     code = "GB" if code.upcase == "UK"
     text = CountryCodes.country(code.upcase) if text.nil?
@@ -45,27 +45,27 @@ module ApplicationHelper
               :title => tooltip_title_attrib(text),
               :style => "vertical-align:middle; #{style}")
   end
-  
+
   def tooltip_title_attrib(text, delay=200)
     return "header=[] body=[#{text}] cssheader=[boxoverTooltipHeader] cssbody=[boxoverTooltipBody] delay=[#{delay}]"
   end
-  
+
   def geo_loc_to_text(geo_loc, style='', flag=true, flag_pos='right')
     return '' if geo_loc.nil?
     return '' if geo_loc.country_code.nil?
-    
+
     text = ''
-    
+
     city, country = BioCatalogue::Util.city_and_country_from_geoloc(geo_loc)
-    
-    unless city.blank? 
+
+    unless city.blank?
       text = text + "#{h(city)}, "
     end
-    
+
     text = text + h(country) unless country.blank?
-    
+
     country_code = h(geo_loc.country_code)
-    
+
     if flag
       case flag_pos.downcase
         when 'right'
@@ -74,52 +74,52 @@ module ApplicationHelper
           text = flag_icon_from_country_code(country_code) + text
         else
           text = text + flag_icon_from_country_code(country_code)
-      end  
+      end
     end
-    
+
     return text
   end
-  
+
   def service_type_badges(service_types)
     html = ''
-    
+
     unless service_types.blank?
       service_types.each do |s_type|
         html = html + content_tag(:span, s_type, :class => "service_type_badge", :style => "vertical-align: middle; margin-left: 0.8em;")
       end
     end
-    
+
     return html
   end
-  
+
   def service_location_flags(service)
     return '' if service.nil?
-    
+
     html = ''
-    
+
     service.service_deployments.each do |s_d|
       unless s_d.country.blank?
         html = html + flag_icon_from_country(s_d.country, s_d.location)
       end
     end
-    
+
     return html
   end
-  
+
   def help_icon_with_tooltip(help_text, delay=200)
     return image_tag("help_icon.png",
                      :title => tooltip_title_attrib(help_text, delay),
                      :style => "vertical-align:middle;")
   end
-  
+
   def info_icon_with_tooltip(info_text, delay=200)
     return image_tag("info.png",
                      :title => tooltip_title_attrib(info_text, delay),
                      :style => "vertical-align:middle;")
   end
-  
+
   # This method is used to generate an icon and/or link that will popup up an in page dialog box for the user to add an annotation (or mutliple annotations at once).
-  # 
+  #
   # It takes in the annotatable object that needs to be annotated and some options (all optional):
   #  :attribute_name - the attribute name for the annotation.
   #    default: nil
@@ -145,42 +145,45 @@ module ApplicationHelper
     # defaults:
     options.reverse_merge!(:attribute_name => nil,
                            :tooltip_text => 'Add annotation',
-                           :style => nil,
+                           :style => 'text-decoration: none;',
+                           :class => nil,
                            :link_text => '',
                            :show_icon => true,
-                           :icon_filename => 'note_add.png',
+                           :icon_filename => 'add_annotation.gif',
+                           :icon_hover_filename => '/images/add_annotation_hover.gif',
                            :show_not_logged_in_text => true,
-                           :multiple => false, 
+                           :multiple => false,
                            :multiple_separator => ',')
-                           
+
     if logged_in?
       link_html = ''
-      link_html = link_html + "<span style='vertical-align:middle'>#{options[:link_text]}</span>" unless options[:link_text].blank?
-      link_html = image_tag(options[:icon_filename], :style => 'vertical-align:middle;margin-right:0.5em;') + link_html if options[:show_icon]
-      
+      link_html = link_html + "<span style='vertical-align:middle; text-decoration: underline;'>#{options[:link_text]}</span>" unless options[:link_text].blank?
+      link_html = image_tag(options[:icon_filename], :mouseover => options[:icon_hover_filename], :style => 'vertical-align:middle;margin-right:0.5em;') + link_html if options[:show_icon]
+
       url_options = { :annotatable_type => annotatable.class.name, :annotatable_id => annotatable.id }
       url_options[:attribute_name] = options[:attribute_name] unless options[:attribute_name].nil?
       url_options[:multiple] = options[:multiple] if options[:multiple]
       url_options[:separator] = options[:multiple_separator] if options[:multiple]
-      
-      return link_to_remote_redbox(link_html, 
+
+      return link_to_remote_redbox(link_html,
                                    { :url => new_popup_annotations_url(url_options),
                                      :id => "annotate_#{annotatable.class.name}_#{annotatable.id}_#{options[:attribute_name]}_redbox",
-                                     :failure => "alert('Sorry, an error has occurred.'); RedBox.close();" }, 
-                                   { :style => options[:style], 
-                                     :alt => options[:tooltip_text], 
+                                     :failure => "alert('Sorry, an error has occurred.'); RedBox.close();" },
+                                   { :style => options[:style],
+                                     :class => options[:class],
+                                     :alt => options[:tooltip_text],
                                      :title => tooltip_title_attrib(options[:tooltip_text]) })
     else
       if options[:show_not_logged_in_text] == true
         return content_tag(:span, "#{link_to("Log in", login_path)} to add #{options[:attribute_name].downcase}", :style => options[:style])
       else
-        return ''        
+        return ''
       end
     end
   end
-  
+
   # This method is used to generate an icon and/or link that will popup up an in page dialog box for the user to edit an annotation.
-  # 
+  #
   # It takes in the annotation object that needs to be edited and some options (all optional):
   #  :tooltip_text - text that will be displayed in a tooltip over the icon/text.
   #    default: 'Edit annotation'
@@ -202,49 +205,49 @@ module ApplicationHelper
                            :link_text => 'edit',
                            :show_icon => false,
                            :icon_filename => 'note_edit.png')
-                           
+
     link_html = ''
     link_html = link_html + "<span style='vertical-align:middle'>#{options[:link_text]}</span>" unless options[:link_text].blank?
     link_html = image_tag(options[:icon_filename], :style => 'vertical-align:middle;margin-right:0.5em;') + link_html if options[:show_icon]
-    
-    return link_to_remote_redbox(link_html, 
+
+    return link_to_remote_redbox(link_html,
                                  { :url => edit_popup_annotation_url(annotation),
                                    :id => "edit_ann_#{annotation.id}_redbox",
-                                   :failure => "alert('Sorry, an error has occurred.'); RedBox.close();" }, 
-                                 { :style => options[:style], 
-                                   :alt => options[:tooltip_text], 
+                                   :failure => "alert('Sorry, an error has occurred.'); RedBox.close();" },
+                                 { :style => options[:style],
+                                   :alt => options[:tooltip_text],
                                    :title => tooltip_title_attrib(options[:tooltip_text]) })
   end
-  
+
   def annotation_add_info_text(attribute_name, annotatable)
     return '' if annotatable.nil?
-    
+
     if attribute_name.blank?
       return "You are adding a custom annotation for the #{annotatable.class.name.titleize}: <b/>#{h(annotatable.annotatable_name)}</b>."
     else
       #return "You are adding a <b>#{attribute_name}</b> for the #{annotatable.class.name.titleize}: <b/>#{annotatable.annotatable_name}</b>"
       return "For #{annotatable.class.name.titleize}: <b/>#{h(annotatable.annotatable_name)}</b>."
     end
-    
+
   end
-  
+
   def annotation_add_value_label(attribute_name, multiple)
     label = ''
-    
+
     if attribute_name.blank?
       label = "Value"
     else
       label = h(attribute_name)
     end
-    
+
     # Pluralise if necessary...
-    label = label.pluralize if multiple 
-    
+    label = label.pluralize if multiple
+
     label = label + ":"
-    
+
     return label
   end
-  
+
   def separator_symbol_to_text(symbol, pluralize_text=false, show_symbol_after=true)
     text = case symbol.to_s
       when ' ' then "space"
@@ -252,47 +255,47 @@ module ApplicationHelper
       when ';' then "semi-colon"
       else symbol
     end
-    
+
     text = text.pluralize if pluralize_text
-    
+
     text = "#{text} ('#{symbol}')" if show_symbol_after
-    
+
     return text
   end
-  
+
   def annotation_source_cssclass(annotation)
     return "annotation_source_#{annotation.source_type.downcase}"
   end
-  
+
   def annotation_source_text(annotation, style='')
     return '' if annotation.nil?
-    
+
     return content_tag(:p, :class => "annotation_source_text #{annotation_source_cssclass(annotation)}", :style => style) do
       "by #{annotation.source_type.titleize.downcase}: <b>#{link_to(h(annotation.source.annotation_source_name), annotation.source)}</b> (#{distance_of_time_in_words_to_now(annotation.created_at)} ago)"
     end
   end
-  
+
   def annotation_prepare_description(desc, do_strip_tags, truncate_length, do_auto_link)
     return '' if desc.nil?
-    
+
     desc = strip_tags(desc) if do_strip_tags
     desc = truncate(desc, :length => truncate_length) unless truncate_length.nil?
     desc = simple_format(desc) unless do_strip_tags
     desc = auto_link(desc, :all, :target => '_blank') if do_auto_link
     desc = white_list(desc)
-      
+
     return desc
   end
-  
+
   # ========================================
   # Code to help with remembering which tab
   # the  user was in after redirects etc.
   # ----------------------------------------
-  
+
   # Note: the implementation of this method means that when it is used
-  # it also resets the param to "false", thus the remembering of a tab is 
-  # only done in the one (current) request. 
-  # If more control than that is required (ie: being able to configure how long tab is remembered for), 
+  # it also resets the param to "false", thus the remembering of a tab is
+  # only done in the one (current) request.
+  # If more control than that is required (ie: being able to configure how long tab is remembered for),
   # then split into 2 different methods.
   def get_and_reset_use_tab_cookie_param_value
     value = session[:use_tab_cookie]
@@ -300,6 +303,6 @@ module ApplicationHelper
     session[:use_tab_cookie] = false
     return value
   end
-  
+
   # ========================================
 end
