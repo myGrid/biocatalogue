4048a482aa1cf1fd810aa047a473ef1f182f16c4
jits <jits@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2009-04-27 10:59:32 +0000

    Commented out some debugging code.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@647 1acaf488-1b9d-4964-8077-0a2fdb83ef00

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -482,18 +482,18 @@ module ApplicationHelper
   # If more control than that is required (ie: being able to configure how long tab is remembered for),
   # then split into 2 different methods.
   def get_and_reset_use_tab_cookie_param_value
-    logger.info ""
-    logger.info "get_and_reset_use_tab_cookie_param_value called; before - session[:use_tab_cookie] = #{session[:use_tab_cookie]}"
-    logger.info ""
+    #logger.info ""
+    #logger.info "get_and_reset_use_tab_cookie_param_value called; before - session[:use_tab_cookie] = #{session[:use_tab_cookie]}"
+    #logger.info ""
     
     value = session[:use_tab_cookie]
     value = value.blank? ? false : value
     
     session[:use_tab_cookie] = false
     
-    logger.info ""
-    logger.info "get_and_reset_use_tab_cookie_param_value called; after - session[:use_tab_cookie] = #{session[:use_tab_cookie]}"
-    logger.info ""
+    #logger.info ""
+    #logger.info "get_and_reset_use_tab_cookie_param_value called; after - session[:use_tab_cookie] = #{session[:use_tab_cookie]}"
+    #logger.info ""
     
     return value
   end
