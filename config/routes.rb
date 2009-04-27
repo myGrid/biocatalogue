fce62db59aff629f1cb91a799cbbdff31e74d493
jits <jits@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2009-04-27 10:40:03 +0000

    Refresh link now does a POST instead of GET, to prevent bots etc from hitting it.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@646 1acaf488-1b9d-4964-8077-0a2fdb83ef00

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@
 ActionController::Routing::Routes.draw do |map|
   # Stats
   map.stats_index '/stats', :controller => 'stats', :action => 'index'
-  map.refresh_stats '/stats/refresh', :controller => 'stats', :action => 'refresh'
+  map.refresh_stats '/stats/refresh', :controller => 'stats', :action => 'refresh', :conditions => { :method => :post }
   
   map.resources :registries
 
