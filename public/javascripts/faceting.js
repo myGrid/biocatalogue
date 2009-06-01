778b85cff75514a4896f6cdc1c93f339d3da13ee
jits <jits@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2009-06-01 10:35:30 +0000

    - Removed redundant faceting.js file. - Added and configured the asset_manager plugin, which compresses all JS and CSS files into in production mode (except in our case the tabber and the widgets JS files), to greatly improve client side performance.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@733 1acaf488-1b9d-4964-8077-0a2fdb83ef00

--- public/javascripts/faceting.js
+++
@@ -1,8 +0,0 @@
-/**
- * BioCatalogue: app/public/javascripts/faceting.js
- *
- * Copyright (c) 2009, University of Manchester, The European Bioinformatics 
- * Institute (EMBL-EBI) and the University of Southampton.
- * See license.txt for details
- */
-
