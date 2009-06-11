23450d308efe61dd3695a7f9909734c3cd3f7ef9
jits <jits@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2009-06-11 12:53:42 +0000

    Minor fix for 'alive' Metal endpoint.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@795 1acaf488-1b9d-4964-8077-0a2fdb83ef00

--- app/metal/alive.rb
+++ app/metal/alive.rb
@@ -1,7 +1,7 @@
 # Allow the metal piece to run in isolation
 #require(File.dirname(__FILE__) + "/../../config/environment") unless defined?(Rails)
 
-class Alive
+class Alive < Rails::Rack::Metal
   def self.call(env)
     if env["PATH_INFO"] =~ /^\/alive/
       [200, {"Content-Type" => "text/html"}, ["I am Alive!"]]
