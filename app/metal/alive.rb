0f8835b7e0c299068b6333ae241cdea5197800fb
jits <jits@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2009-06-11 12:41:56 +0000

    Added Metal endpoint 'alive' for things like the the load balancers to check if the app is running.
    Use the URL: http://{server_name}:{port}/alive to call this.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@794 1acaf488-1b9d-4964-8077-0a2fdb83ef00

---
+++ app/metal/alive.rb
@@ -0,0 +1,12 @@
+# Allow the metal piece to run in isolation
+#require(File.dirname(__FILE__) + "/../../config/environment") unless defined?(Rails)
+
+class Alive
+  def self.call(env)
+    if env["PATH_INFO"] =~ /^\/alive/
+      [200, {"Content-Type" => "text/html"}, ["I am Alive!"]]
+    else
+      [404, {"Content-Type" => "text/html"}, ["Not Found"]]
+    end
+  end
+end
