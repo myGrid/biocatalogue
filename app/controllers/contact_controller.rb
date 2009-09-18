5c29fdd3e7d281c21e870594681f35085e0ad295
tlaurent <tlaurent@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2009-09-18 15:10:17 +0000

    added a security check against spam bot for the contact form
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@1086 1acaf488-1b9d-4964-8077-0a2fdb83ef00

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,6 +1,6 @@
 # BioCatalogue: app/controllers/contact_controller.rb
 #
-# Copyright (c) 2008, University of Manchester, The European Bioinformatics 
+# Copyright (c) 2008, University of Manchester, The European Bioinformatics
 # Institute (EMBL-EBI) and the University of Southampton.
 # See license.txt for details.
 
@@ -11,24 +11,24 @@ class ContactController < ApplicationController
       format.html # index.html.erb
     end
   end
-  
+
   def create
     from_user = params[:from] || current_user.try(:display_name) || "no name specified"
     from_user += ' (' + (params[:email] || current_user.try(:email) || 'no email specified') + ')'
-    
-    if verify_recaptcha
+
+    if params[:hidden_feedback_length] == params[:length_check]
       ContactMailer.deliver_feedback(from_user, params[:subject], params[:content])
-  
+
       respond_to do |format|
         flash[:notice] = 'Your message has been submitted. Thank you very much.'
         format.html { redirect_to contact_url }
       end
     else
       respond_to do |format|
-        flash[:error] = 'Your message has not been submitted. CAPTCHA was not entered correctly.'
+        flash[:error] = 'Your message has not been submitted. The message length was not entered correctly.'
         format.html { render :action => :index }
       end
     end
-    
+
   end
 end
