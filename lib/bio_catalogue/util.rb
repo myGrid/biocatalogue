90c8e5752d317912ad1db8e90d758e86d4faae2e
jits <jits@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2009-10-28 15:26:49 +0000

    Added the ability to switch between simple/detailed views of search results.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@1214 1acaf488-1b9d-4964-8077-0a2fdb83ef00

--- lib/bio_catalogue/util.rb
+++ lib/bio_catalogue/util.rb
@@ -206,10 +206,10 @@ module BioCatalogue
     #
     # NOTE: the provided params collection will not be affected. 
     # Instead, a new hash will be returned. 
-    def self.remove_rails_special_params_from(params)
+    def self.remove_rails_special_params_from(params, additional_to_remove=[])
       return { } if params.blank?
       
-      special_params = %w( id format controller action )
+      special_params = %w( id format controller action commit ).concat(additional_to_remove)
       return params.reject { |k,v| special_params.include?(k.to_s.downcase) }
     end
     
