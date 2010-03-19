24ea77a0f59255788cd0e8cb13560744ad9d3c6d
jits <jits@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2010-03-19 18:41:11 +0000

    More work on the new UI for SOAP operations and REST endpoints.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@1725 1acaf488-1b9d-4964-8077-0a2fdb83ef00

--- app/helpers/rest_methods_helper.rb
+++ app/helpers/rest_methods_helper.rb
@@ -73,49 +73,4 @@ module RestMethodsHelper
     return link_content
   end
   
-  
-  
-  # This method will create an dropdown title (in the form of a link) for 
-  # RestParameters, and RestRepresentations which allows the components to be 
-  # expanded or collapsed.
-  #
-  # CONFIGURATION OPTIONS (all these options are optional)
-  #  :link_text - text to be displayed as part of the link.
-  #    default: update_element_id (the ID of the element to be expanded or collapsed)
-  #  :class - any CSS class that need to be applied to the text.
-  #    default: nil
-  #  :icon_left_margin - the left margin of the expand / collapse icon
-  #    default: "5px"
-  #  :icon_float - the CSS float value for the icon i.e. 'left', right', etc.  This OVERRIDES :icon_left_margin
-  #    default: ''
-def create_expand_collapse_link(update_element_id, *args)
-    return '' if update_element_id.blank?
-    
-    options = args.extract_options!
-    # default config options
-    options.reverse_merge!(:link_text => update_element_id,
-                           :class => nil,
-                           :icon_left_margin => "5px",
-                           :icon_float => "")
-
-    expand_link = options[:link_text] + expand_image(options[:icon_left_margin], options[:icon_float])
-    collapse_link = options[:link_text] + collapse_image(options[:icon_left_margin], options[:icon_float])
-
-    expand_link_id = update_element_id + '_name_more_link'
-    collapse_link_id = update_element_id + '_name_less_link'
-    
-    expand_link_content = link_to_function(expand_link, :id => expand_link_id) do |page| 
-                            page.toggle expand_link_id, collapse_link_id
-                            page.visual_effect :toggle_blind, update_element_id, :duration => '0.2'
-                          end
-
-    collapse_link_content = link_to_function(collapse_link, :id => collapse_link_id, :style => "display:none;") do |page| 
-                              page.toggle expand_link_id, collapse_link_id
-                              page.visual_effect :toggle_blind, update_element_id, :duration => '0.2'
-                            end 
-                            
-    span_content = expand_link_content + collapse_link_content
-
-    return content_tag(:span, span_content, :class => options[:class])
-  end
 end
