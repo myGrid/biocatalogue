0b9c2e53c57b36feced6c2aa8a50dca715a54df2
jits <jits@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2010-05-24 17:05:36 +0000

    Fix to the ActivityLog records for 'status_change' events - previously the 'referenced' was not being set to the associated Service, so activity feeds could not pick them up. This will ensure that all previous records are fixed.
    Note: that a fix was made previously to the status_change logging to ensure that the referenced gets set appropriately.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@2031 1acaf488-1b9d-4964-8077-0a2fdb83ef00

---
+++ script/biocatalogue/fix_activity_logs_for_status_change_may10.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+# BioCatalogue: script/biocatalogue/fix_activity_logs_for_status_change_may10.rb
+#
+# Copyright (c) 2010, University of Manchester, The European Bioinformatics 
+# Institute (EMBL-EBI) and the University of Southampton.
+# See license.txt for details
+
+environment = (ARGV[0].nil? || ARGV[0]=='' ? "development" : ARGV[0])
+RAILS_ENV = environment
+
+# Load up the Rails app
+require File.join(File.dirname(__FILE__), '..', '..', 'config', 'environment')
+
+begin
+  
+  ActivityLog.record_timestamps = false
+  
+  ActivityLog.transaction do 
+    
+    ActivityLog.find(:all, :conditions => { :action => 'status_change' }).each do |al|
+      if al.activity_loggable and al.referenced_id.nil?
+        al.referenced = al.activity_loggable.service
+        al.save!
+      end
+    end
+    
+  end
+  
+  ActivityLog.record_timestamps = true
+  
+rescue Exception => ex
+  puts ""
+  puts "> ERROR: exception occured and transaction has been rolled back. Exception:"
+  puts ex.message
+  puts ex.backtrace.join("\n")
+end
