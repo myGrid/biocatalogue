1fd52c2bb32c2184fc205c4e198e8682610810da
ebontane <ebontane@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2011-01-26 11:46:24 +0000

    background job to send a message to members
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@2342 1acaf488-1b9d-4964-8077-0a2fdb83ef00

---
+++ lib/bio_catalogue/jobs/member_info_email.rb
@@ -0,0 +1,20 @@
+# BioCatalogue: lib/bio_catalogue/jobs/status_change_emails
+#
+# Copyright (c) 2009, University of Manchester, The European Bioinformatics 
+# Institute (EMBL-EBI) and the University of Southampton.
+# See license.txt for details
+
+module BioCatalogue
+  module Jobs
+    class MemberInfoEmail < Struct.new(:subject, :text, :email)
+      def perform
+          begin
+          	MemberInfoMailer.deliver_text_to_email(subject, text, email) 
+         	rescue Exception => ex
+          	Rails.logger.error("Failed to deliver email to #{email}") 
+            Rails.logger.error(ex.to_s)
+         	end
+      end   
+    end
+  end
+end
