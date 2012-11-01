ea2bb8a5ebd6a3ff5008b79b18218ee5b889a21b
rhaines <rhaines@1acaf488-1b9d-4964-8077-0a2fdb83ef00>
2012-11-01 13:33:00 +0000

    Added colour customization using semi-transparent images and rejigging CSS
    git-svn-id: svn+ssh://rubyforge.org/var/svn/biocatalogue/trunk@2549 1acaf488-1b9d-4964-8077-0a2fdb83ef00

--- vendor/plugins/rounded_corners_helper/lib/rounded_corners_helper.rb
+++ vendor/plugins/rounded_corners_helper/lib/rounded_corners_helper.rb
@@ -1,21 +1,22 @@
 module RoundedCornersHelper
   def rounded(background, color, width, &block)
 		output = self
+    style = "style=\"#{"background:#{background};" if background} color:#{color}\""
 	  output.concat "<div class=\"rcontainer\" style=\"width:#{width};\">"
 	  output.concat "<div class=\"rtop\">"
-	  output.concat "<div class=\"r1\" style=\"background:#{background};\"></div>"
-	  output.concat "<div class=\"r2\" style=\"background:#{background};\"></div>"
-	  output.concat "<div class=\"r3\" style=\"background:#{background};\"></div>"
-	  output.concat "<div class=\"r4\" style=\"background:#{background};\"></div>"
+    output.concat "<div class=\"r1\" #{style}></div>"
+    output.concat "<div class=\"r2\" #{style}></div>"
+    output.concat "<div class=\"r3\" #{style}></div>"
+    output.concat "<div class=\"r4\" #{style}></div>"
 	  output.concat "</div>"
-	  output.concat "<div class=\"rcontain\" style=\"background:#{background};color:#{color};\">"
+	  output.concat "<div class=\"rcontain\" #{style}>"
 		block.call
 		output.concat "</div>"
 	  output.concat "<div class=\"rbottom\">"
-	  output.concat "<div class=\"r4\" style=\"background:#{background};\"></div>"
-	  output.concat "<div class=\"r3\" style=\"background:#{background};\"></div>"
-	  output.concat "<div class=\"r2\" style=\"background:#{background};\"></div>"
-	  output.concat "<div class=\"r1\" style=\"background:#{background};\"></div>"
+    output.concat "<div class=\"r4\" #{style}></div>"
+    output.concat "<div class=\"r3\" #{style}></div>"
+    output.concat "<div class=\"r2\" #{style}></div>"
+    output.concat "<div class=\"r1\" #{style}></div>"
 	  output.concat "</div>"
 	  output.concat "</div>"
 	end
@@ -23,23 +24,25 @@ module RoundedCornersHelper
   # Added by Jits (2009-11-26)
   def rounded_html(background, color, width, &block)
     output = ""
+    style = "style=\"#{"background:#{background};" if background} color:#{color}\""
     output.concat "<div class=\"rcontainer\" style=\"width:#{width};\">"
     output.concat "<div class=\"rtop\">"
-    output.concat "<div class=\"r1\" style=\"background:#{background};\"></div>"
-    output.concat "<div class=\"r2\" style=\"background:#{background};\"></div>"
-    output.concat "<div class=\"r3\" style=\"background:#{background};\"></div>"
-    output.concat "<div class=\"r4\" style=\"background:#{background};\"></div>"
+    output.concat "<div class=\"r1\" #{style}></div>"
+    output.concat "<div class=\"r2\" #{style}></div>"
+    output.concat "<div class=\"r3\" #{style}></div>"
+    output.concat "<div class=\"r4\" #{style}></div>"
     output.concat "</div>"
-    output.concat "<div class=\"rcontain\" style=\"background:#{background};color:#{color};\">"
+    output.concat "<div class=\"rcontain\" #{style}>"
     output.concat block.call
     output.concat "</div>"
     output.concat "<div class=\"rbottom\">"
-    output.concat "<div class=\"r4\" style=\"background:#{background};\"></div>"
-    output.concat "<div class=\"r3\" style=\"background:#{background};\"></div>"
-    output.concat "<div class=\"r2\" style=\"background:#{background};\"></div>"
-    output.concat "<div class=\"r1\" style=\"background:#{background};\"></div>"
+    output.concat "<div class=\"r4\" #{style}></div>"
+    output.concat "<div class=\"r3\" #{style}></div>"
+    output.concat "<div class=\"r2\" #{style}></div>"
+    output.concat "<div class=\"r1\" #{style}></div>"
     output.concat "</div>"
     output.concat "</div>"
     output
   end
+
 end
