4ea695424cc0fd6cb599410b7329cadb2a5a8a07
niall beard <niallbeard@gmail.com>
2013-06-04 22:00:22 +0100

    Disabled action_view.debug_jrs as it was causing errors when running server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Catalographer::Application.configure do
+BioCatalogue::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
@@ -11,7 +11,7 @@ Catalographer::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_view.debug_rjs             = true
+  #config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
@@ -22,5 +22,6 @@ Catalographer::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
 end
 
