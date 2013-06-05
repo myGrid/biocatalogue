5ff3d2a8332c9e5d06aca6df030dfe742a7304c6
anenadic <anenadic@cs.man.ac.uk>
2013-06-05 15:53:31 +0100

    Deleted config/initializers/secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,9 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-
-#Be sure to change your secret token when you run your own deployment.
-BioCatalogue::Application.config.secret_token = '937d78f349945f434a8427f2449351e442341f4164df79de10a3782831dabf35aaa8c99869ef338ac3e25ca1742422e0e372514099b21b82db255390352530dd'
