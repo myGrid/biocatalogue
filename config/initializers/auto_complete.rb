4690e5831b153728030bb6a86acc537c8db02e88
niall <niallbeard@gmail.com>
2013-07-03 12:12:21 +0100

    Folded autocomplete plugin into the lib and config/initializers folders

--- config/initializers/auto_complete.rb
+++ config/initializers/auto_complete.rb
@@ -1,2 +1,4 @@
+require 'auto_complete/lib/auto_complete'
+require 'auto_complete/lib/auto_complete_macros_helper'
 ActionController::Base.send :include, AutoComplete
 ActionController::Base.helper AutoCompleteMacrosHelper
