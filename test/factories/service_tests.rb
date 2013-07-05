06b5b6d96d7e794f70090be3e633e3800d702456
Finn Bacall <finn.bacall@cs.man.ac.uk>
2013-07-05 16:31:53 +0100

    Fixed ActiveRecord::RecordNotSaved: Failed to save the new associated service_test

--- test/factories/service_tests.rb
+++ test/factories/service_tests.rb
@@ -7,7 +7,7 @@ Factory.define :service_test do |stest|
 end
 
 Factory.define :script_service_test, :parent => :service_test do |service_test|
- service_test.test {|t| t.association(:test_script)}
+ service_test.after_create {|st| st.test {|t| t.association(:test_script)}}
 end
 
 Factory.define :script_service_test_with_result, :parent => :script_service_test do |service_test|
