635563f07d2dd9663836c5eb4fb00afae65fa401
anenadic <anenadic@cs.man.ac.uk>
2013-07-08 16:54:21 +0100

    Added a small setup script that can be used after cloning to copy the .pre config files and switch to an appropriate git branch.

---
+++ setup_script.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+APP_DIR=`pwd`
+
+GIT_REMOTE=origin
+LOCAL_BRANCH=rails3
+REMOTE_BRANCH=${GIT_REMOTE}/${LOCAL_BRANCH}
+
+branch_name="$(git symbolic-ref HEAD 2>/dev/null)"
+branch_name=${branch_name##refs/heads/}
+
+git show-branch $LOCAL_BRANCH 2> /dev/null
+if [ $? != 0 ]  # if $LOCAL_BRANCH branch does not exists
+then
+    echo "Creating new local branch $LOCAL_BRANCH to track $REMOTE_BRANCH..."
+    git branch --track $LOCAL_BRANCH $REMOTE_BRANCH
+fi
+
+echo "Switching to branch $LOCAL_BRANCH"
+git checkout $LOCAL_BRANCH
+
+echo "Running bundle install..."
+bundle install
+
+[ ! -f ${APP_DIR}/config/database.yml ] && cp ${APP_DIR}/config/database.yml.pre ${APP_DIR}/config/database.yml && echo "Created ${APP_DIR}/config/database.yml"
+[ ! -f ${APP_DIR}/config/memcache.yml ] && cp ${APP_DIR}/config/memcache.yml.pre ${APP_DIR}/config/memcache.yml  && echo "Created ${APP_DIR}/config/memcache.yml"
+[ ! -f ${APP_DIR}/config/solr.yml ] && cp ${APP_DIR}/config/solr.yml.pre ${APP_DIR}/config/solr.yml  && echo "Created ${APP_DIR}/config/solr.yml"
+[ ! -f ${APP_DIR}/config/initializers/biocat_local.rb ] && cp ${APP_DIR}/config/initializers/biocat_local.rb.pre ${APP_DIR}/config/initializers/biocat_local.rb && echo "Created ${APP_DIR}/config/initializers/biocat_local.rb"
+[ ! -f ${APP_DIR}/config/initializers/mail.rb ] && cp ${APP_DIR}/config/initializers/mail.rb.pre ${APP_DIR}/config/initializers/mail.rb && echo "Created ${APP_DIR}/config/initializers/mail.rb"
+[ ! -f ${APP_DIR}/config/initializers/secret_token.rb ] && cp ${APP_DIR}/config/initializers/secret_token.rb.pre ${APP_DIR}/config/initializers/secret_token.rb && echo "Created ${APP_DIR}/config/initializers/secret_token.rb"
+[ ! -f ${APP_DIR}/data/service_categories.yml ] && cp ${APP_DIR}/data/service_categories.yml.pre ${APP_DIR}/data/service_categories.yml && echo "Created ${APP_DIR}/data/service_categories.yml"
+[ ! -f ${APP_DIR}/public/stylesheets/colour-override.css ] && cp ${APP_DIR}/public/stylesheets/colour-override.css.pre ${APP_DIR}/public/stylesheets/colour-override.css && echo "Created ${APP_DIR}/public/stylesheets/colour-override.css"
+
+echo
+echo "Please remember to configure the following to suit your environment:" 
+echo "config/database.yml"
+echo "config/memcache.yml"
+echo "config/solr.yml"
+echo "config/initializers/biocat_local.rb"
+echo "config/initializers/mail.rb"
+echo "config/initializers/secret_token.rb"
+echo "data/service_categories.yml"
+echo "public/stylesheets/colour-override.css"
+
+echo "Done."
+exit 0
+
+
