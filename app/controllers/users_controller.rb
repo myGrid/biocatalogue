myGrid/biocatalogue

commit 4a2c05446f9245dfa828f4f8dc6655180f7ec1cb

Replaced deprecated methods such as find(:all), find(:first), finds with conditions, and the :joins.

app/controllers/users_controller.rb
@@ -513,7 +513,9 @@ private
   end
   
   def find_user
-    @user = User.find(params[:id], :conditions => "activated_at IS NOT NULL")
+    # Old Rails 2 style
+    #@user = User.find(params[:id], :conditions => "activated_at IS NOT NULL")
+    @user = User.find(params[:id]).where("activated_at IS NOT NULL")
   end
 
   def find_user_inclusive

app/controllers/oauth_clients_controller.rb
@@ -16,7 +16,9 @@ class OauthClientsController < ApplicationController
    
   def index
     @client_applications = current_user.client_applications
-    @tokens = current_user.tokens.find :all, :conditions => 'oauth_tokens.invalidated_at is null and oauth_tokens.authorized_at is not null'
+    # Old Rails 2 style
+    #@tokens = current_user.tokens.find :all, :conditions => 'oauth_tokens.invalidated_at is null and oauth_tokens.authorized_at is not null'
+    @tokens = current_user.tokens.where('oauth_tokens.invalidated_at is null and oauth_tokens.authorized_at is not null')
   end
 
   def new

app/controllers/rest_methods_controller.rb
@@ -327,7 +327,9 @@ private # ========================================
   end
 
   def find_rest_method
-    @rest_method = RestMethod.find(params[:id], :include => :rest_resource)
+    #Old Rails 2 style
+    #@rest_method = RestMethod.find(params[:id], :include => :rest_resource)
+    @rest_method = RestMethod.find(params[:id]).includes(:rest_resource)
   end
   
   def find_rest_methods
@@ -388,10 +390,14 @@ private # ========================================
   def destroy_unused_objects(id_list, is_parameter=true)
     id_list.sort.each do |obj_id|
       if is_parameter
-        not_used = RestMethodParameter.all(:conditions => {:rest_parameter_id => obj_id}).empty?
+        # Old Rails 2 style
+        #not_used = RestMethodParameter.all(:conditions => {:rest_parameter_id => obj_id}).empty?
+        not_used = RestMethodParameter.where(:rest_parameter_id => obj_id).empty?
         RestParameter.destroy(obj_id) if not_used
       else
-        not_used = RestMethodRepresentation.all(:conditions => {:rest_representation_id => obj_id}).empty?
+        # Old Rails 2 style
+        #not_used = RestMethodRepresentation.all(:conditions => {:rest_representation_id => obj_id}).empty?
+        not_used = RestMethodRepresentation.where(:rest_representation_id => obj_id).empty?
         RestRepresentation.destroy(obj_id) if not_used      
       end
     end # id_list.each

app/controllers/service_providers_controller.rb
@@ -146,10 +146,12 @@ class ServiceProvidersController < ApplicationController
 
   def auto_complete
     @name_fragment = params[:name] || ''
-    
-    @results = ServiceProvider.all(
-                                    :conditions => "name like '%" + @name_fragment.downcase + "%'")
-                                    
+
+    # Old Rails 2 style
+    #@results = ServiceProvider.all(
+    #                                :conditions => "name like '%" + @name_fragment.downcase + "%'")
+    @results = ServiceProvider.where("name like '%" + @name_fragment.downcase + "%'")
+
     render :inline => "<%= auto_complete_result @results, 'name', @name_fragment %>", :layout => false
   end
 

app/controllers/soap_operations_controller.rb
@@ -155,7 +155,9 @@ protected
   end
   
   def find_soap_operation
-    @soap_operation = SoapOperation.find(params[:id], :include => [ :soap_inputs, :soap_outputs ])
+    # Old Rails 2 style
+    #@soap_operation = SoapOperation.find(params[:id], :include => [ :soap_inputs, :soap_outputs ])
+    @soap_operation = SoapOperation.find(params[:id]).includes(:soap_inputs, :soap_outputs)
   end
   
 private
@@ -169,13 +171,17 @@ private
     # Now add any other filters, if specified by "also=..."
     
     if @api_params[:also].include?('inputs')
-      @soap_operation.soap_inputs.all(:select => "id").each do |input|
-        new_params = BioCatalogue::Filtering.add_filter_to_params(new_params, :asin, input.id)
+      # Old Rails 2 style
+      #@soap_operation.soap_inputs.all(:select => "id").each do |input|
+      @soap_operation.soap_inputs.select("id").each do |input|
+          new_params = BioCatalogue::Filtering.add_filter_to_params(new_params, :asin, input.id)
       end
     end
     
     if @api_params[:also].include?('outputs')
-      @soap_operation.soap_outputs.all(:select => "id").each do |output|
+      # Old Rails 2 style
+      #@soap_operation.soap_outputs.all(:select => "id").each do |output|
+      @soap_operation.soap_outputs.select("id").each do |output|
         new_params = BioCatalogue::Filtering.add_filter_to_params(new_params, :asout, output.id)
       end
     end

app/models/service.rb
@@ -72,8 +72,10 @@ class Service < ActiveRecord::Base
   end
   
   def self.latest(limit=10)
-    self.all(              :order => "created_at DESC",
-              :limit => limit)
+    # Old Rails 2 style
+    #self.all(              :order => "created_at DESC",
+    #                       :limit => limit)
+    self.order("created_at DESC").limit(limit)
   end
   
   def to_json
@@ -105,9 +107,11 @@ class Service < ActiveRecord::Base
   end
   
   def service_version_instances_by_type(type_model_name)
-    type_model_name.constantize.all(                                     :conditions => { :service_versions => { :service_id => self.id } },
-                                     :joins => [ :service_version ])
-  end 
+    # Old Rails 2
+    #type_model_name.constantize.all(                                     :conditions => { :service_versions => { :service_id => self.id } },
+    #                                 :joins => [ :service_version ])
+    type_model_name.constantize.joins(:service_version).where(:service_versions => {:service_id => self.id})
+  end
   
   # Gets an array of all the service types that this service has (as part of it's versions).
   def service_types
@@ -183,8 +187,10 @@ class Service < ActiveRecord::Base
       end
       
       service_ids = service_ids.uniq.reject{|i| i == self.id}
-      
-      services = Service.all(:conditions => { :id => service_ids[0...limit] })
+
+      # Old Rails 2 style
+      #services = Service.all(:conditions => { :id => service_ids[0...limit] })
+      services = Service.where(:id => service_ids[0...limit])
     end
     
     return services
@@ -193,11 +199,16 @@ class Service < ActiveRecord::Base
   # IF this is Service is part of a Soaplab Server then this method returns that SoaplabServer entry.
   # Otherwise it returns nil, which indicates that this Service is not part of a Soaplab Server.
   def soaplab_server
-    rel = Relationship.first(
-                            :conditions => { :subject_type => "Service", 
+    # Old Rails 2 style
+    #rel = Relationship.first(
+    #    :conditions => { :subject_type => "Service",
+    #                     :subject_id => self.id,
+    #                     :predicate => "BioCatalogue:memberOf",
+    #                     :object_type => "SoaplabServer" })
+    rel = Relationship.where(:subject_type => "Service",
                                              :subject_id => self.id, 
                                              :predicate => "BioCatalogue:memberOf", 
-                                             :object_type => "SoaplabServer" })
+                                             :object_type => "SoaplabServer").first
     rel.nil? ? rel : rel.object
   end
   
@@ -210,7 +221,9 @@ class Service < ActiveRecord::Base
   end
   
   def service_tests_by_type(type)
-    ServiceTest.all(:conditions => {:test_type => type, :service_id => self.id})
+    # Old Rails 2 style
+    #ServiceTest.all(:conditions => {:test_type => type, :service_id => self.id})
+    ServiceTest.where(:test_type => type, :service_id => self.id)
   end
   
   # e.g. To find all test scripts:  service_test_instances_by_type('TestScript')
@@ -274,7 +287,9 @@ class Service < ActiveRecord::Base
   # service by adding them to the set of those responsible to
   # manage the service the service by default.  
   def all_responsibles
-    curators = User.all(:conditions => {:role_id => [ 1, 2 ]})
+    # Old Rails 2 style
+    #curators = User.all(:conditions => {:role_id => [ 1, 2 ]})
+    curators = User.where(:role_id => [ 1, 2 ])
     responsibles = self.service_responsibles.collect{|r| r.user if r.status='active'}.compact
     responsibles << self.submitter if self.submitter_type == "User"
     responsibles.concat(curators)
@@ -282,9 +297,13 @@ class Service < ActiveRecord::Base
   end
   
   def pending_responsibility_requests(limit=5)
-    reqs = ResponsibilityRequest.all(:conditions => {:subject_type => self.class.name,
-                                                            :subject_id => self.id,
-                                                            :status => 'pending'}, :limit => limit)
+    # Old Rails 2 style
+    #reqs = ResponsibilityRequest.all(:conditions => {:subject_type => self.class.name,
+    #                                                        :subject_id => self.id,
+    #                                                        :status => 'pending'}, :limit => limit)
+    reqs = ResponsibilityRequest.where(:subject_type => self.class.name,
+                                           :subject_id => self.id,
+                                           :status => 'pending').limit(limit)
     return reqs
   end
   
@@ -339,27 +358,45 @@ class Service < ActiveRecord::Base
   
   def annotations_activity_logs(since, limit=100)
     obj_ids = self.associated_object_ids
-    
-    ActivityLog.all(      :conditions => [ "action = 'create' AND activity_loggable_type = 'Annotation' AND (
+    # Old Rails 2 style
+    #ActivityLog.all(      :conditions => [ "action = 'create' AND activity_loggable_type = 'Annotation' AND (
+    #                   (activity_loggable_type = 'Service' AND activity_loggable_id = ?) OR (referenced_type = 'Service' AND referenced_id = ?) OR
+    #                   (activity_loggable_type = 'ServiceDeployment' AND activity_loggable_id IN (?)) OR (referenced_type = 'ServiceDeployment' AND referenced_id IN (?)) OR
+    #                   (activity_loggable_type = 'ServiceVersion' AND activity_loggable_id IN (?)) OR (referenced_type = 'ServiceVersion' AND referenced_id IN (?)) OR
+    #                   (activity_loggable_type = 'SoapService' AND activity_loggable_id IN (?)) OR (referenced_type = 'SoapService' AND referenced_id IN (?)) OR
+    #                   (activity_loggable_type = 'RestService' AND activity_loggable_id IN (?)) OR (referenced_type = 'RestService' AND referenced_id IN (?))
+    #                   ) AND created_at >= ?",
+    #                   "Service",
+    #                   self.id,
+    #                   (obj_ids["ServiceDeployments"] || []),
+    #                   (obj_ids["ServiceDeployments"] || []),
+    #                   (obj_ids["ServiceVersions"] || []),
+    #                   (obj_ids["ServiceVersions"] || []),
+    #                   (obj_ids["SoapServices"] || []),
+    #                   (obj_ids["SoapServices"] || []),
+    #                   (obj_ids["RestServices"] || []),
+    #                   (obj_ids["RestServices"] || []),
+    #                   since ],
+    #  :order => "created_at DESC",
+    #  :limit => limit)
+    ActivityLog.where("action = 'create' AND activity_loggable_type = 'Annotation' AND (
                        (activity_loggable_type = 'Service' AND activity_loggable_id = ?) OR (referenced_type = 'Service' AND referenced_id = ?) OR
                        (activity_loggable_type = 'ServiceDeployment' AND activity_loggable_id IN (?)) OR (referenced_type = 'ServiceDeployment' AND referenced_id IN (?)) OR
                        (activity_loggable_type = 'ServiceVersion' AND activity_loggable_id IN (?)) OR (referenced_type = 'ServiceVersion' AND referenced_id IN (?)) OR
                        (activity_loggable_type = 'SoapService' AND activity_loggable_id IN (?)) OR (referenced_type = 'SoapService' AND referenced_id IN (?)) OR
                        (activity_loggable_type = 'RestService' AND activity_loggable_id IN (?)) OR (referenced_type = 'RestService' AND referenced_id IN (?))
-                       ) AND created_at >= ?", 
-                       "Service", 
-                       self.id,
-                       (obj_ids["ServiceDeployments"] || []),
-                       (obj_ids["ServiceDeployments"] || []),
-                       (obj_ids["ServiceVersions"] || []),
-                       (obj_ids["ServiceVersions"] || []),
-                       (obj_ids["SoapServices"] || []),
-                       (obj_ids["SoapServices"] || []),
-                       (obj_ids["RestServices"] || []),
-                       (obj_ids["RestServices"] || []),
-                       since ],
-      :order => "created_at DESC",
-      :limit => limit)
+                       ) AND created_at >= ?",
+                                           "Service",
+                                           self.id,
+                                           (obj_ids["ServiceDeployments"] || []),
+                                           (obj_ids["ServiceDeployments"] || []),
+                                           (obj_ids["ServiceVersions"] || []),
+                                           (obj_ids["ServiceVersions"] || []),
+                                           (obj_ids["SoapServices"] || []),
+                                           (obj_ids["SoapServices"] || []),
+                                           (obj_ids["RestServices"] || []),
+                                           (obj_ids["RestServices"] || []),
+                                           since).order("created_at DESC").limit(limit)
   end
   
   def activate_service_tests!

app/models/test_script.rb
@@ -71,10 +71,12 @@ class TestScript < ActiveRecord::Base
   # Helper class method to lookup all tests assigned
   # to all testable types for a given user.
   def self.find_tests_by_user(user)
-    find(:all,
-      :conditions => ["submitter_id = ?", user.id],
-      :order => "created_at DESC"
-    )
+    # Old Rails 2 style
+    #find(:all,
+    #  :conditions => ["submitter_id = ?", user.id],
+    #  :order => "created_at DESC"
+    #)
+    find.where(":submitter_id = ?", user.id).order("created_at DESC")
   end
   
   # Create an entry into the content_blobs table

app/models/user.rb
@@ -84,7 +84,9 @@ class User < ActiveRecord::Base
     return nil if login.blank? or password.blank?
 
     # Check for a User with email matching 'login'
-    u = find(:first, :conditions => ["email = ?", login])
+    # Old Rails 2 style
+    #u = find(:first, :conditions => ["email = ?", login])
+    u = where("email = ?", login).first
 
     u && u.activated? && u.authenticated?(password) ? u : nil
   end
@@ -249,8 +251,10 @@ class User < ActiveRecord::Base
   
   # TODO: see #other_services_responsible; need to clarify how these two methods are related and possibly combine/separate out the logic somewhere else.
   def active_services_responsible_for
-    Service.all(                 :joins => [ :service_responsibles ],
-                 :conditions => [ "service_responsibles.user_id = ? AND service_responsibles.status = 'active'", self.id ])
+    # Old Rails 2 style
+    #Service.all(                 :joins => [ :service_responsibles ],
+    #                             :conditions => [ "service_responsibles.user_id = ? AND service_responsibles.status = 'active'", self.id ])
+    Service.joins(:service_responsibles).where(["service_responsibles.user_id = ? AND service_responsibles.status = 'active'", self.id ])
   end
   
   def is_admin?

commit 3b9cab7f4e42250dc38c2ccff29e5bc48be9d707

Fix for a bug in find_user introduced by 4a2c05446f9245dfa828f4f8dc6655180f7ec1cb - changed the order of find and where clauses.

app/controllers/users_controller.rb
@@ -515,7 +515,7 @@ private
   def find_user
     # Old Rails 2 style
     #@user = User.find(params[:id], :conditions => "activated_at IS NOT NULL")
-    @user = User.find(params[:id]).where("activated_at IS NOT NULL")
+    @user = User.where("activated_at IS NOT NULL").find(params[:id])
   end
 
   def find_user_inclusive
@@ -526,7 +526,7 @@ private
     find_user if !defined?(@user) or @user.nil?
     unless mine?(@user)     
       respond_to do |format|
-        flash[:error] = "You don't have the rights to perform this action."
+        flash[:error] = "You do not have the rights to perform this action."
         format.html { redirect_to :users }
         format.xml  { redirect_to :users => @user.errors, :status => :unprocessable_entity }
         format.json  { error_to_back_or_home("You are not allowed to perform this action") }