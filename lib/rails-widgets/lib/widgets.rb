ea060f4eae2aaf0ea7bdca91e8e798aa7ac89ea4
Niall Beard <niallbeard@gmail.com>
2013-08-09 13:50:07 +0100

    Changed require statements to not use paths relative to the apps root directory

--- lib/rails-widgets/lib/widgets.rb
+++ lib/rails-widgets/lib/widgets.rb
@@ -1,26 +1,30 @@
 # Widgets
-widgets_path = 'lib/rails-widgets/lib/widgets/'
+widgets_path = '../widgets/'
 
-require widgets_path + 'core'
-require widgets_path + 'css_template'
-require widgets_path + 'highlightable'
-require widgets_path + 'disableable'
+require File.expand_path(widgets_path + 'core', __FILE__)
+require File.expand_path(widgets_path + 'css_template', __FILE__)
+require File.expand_path(widgets_path + 'highlightable', __FILE__)
 
+require File.expand_path(widgets_path + 'disableable', __FILE__)
 ##### Navigation #####
-require widgets_path + 'navigation_item'
-require widgets_path + 'navigation'
-require widgets_path + 'navigation_helper'
+require File.expand_path(widgets_path + 'navigation_item', __FILE__)
+require File.expand_path(widgets_path + 'navigation', __FILE__)
+require File.expand_path(widgets_path + 'navigation_helper', __FILE__)
 ActionController::Base.helper Widgets::NavigationHelper
 
+
 ##### Tabnav #####
-require widgets_path + 'tab'
-require widgets_path + 'tabnav'
-require widgets_path + 'tabnav_helper'
+require File.expand_path(widgets_path + 'tab', __FILE__)
+require File.expand_path(widgets_path + 'tabnav', __FILE__)
+require File.expand_path(widgets_path + 'tabnav_helper', __FILE__)
 ActionController::Base.helper Widgets::TabnavHelper
 
+
 ##### Table #####
-require widgets_path + 'table_helper'
+require File.expand_path(widgets_path + 'table_helper', __FILE__)
 ActionController::Base.helper Widgets::TableHelper
+
+
 
 ##### Code #####
 # not enabled by default because it depends on the Syntax gem
@@ -28,22 +32,25 @@ ActionController::Base.helper Widgets::TableHelper
 # ActionController::Base.helper Widgets::CodeHelper
 
 ##### ShowHide #####
-require widgets_path + 'showhide_helper'
+require File.expand_path(widgets_path + 'showhide_helper', __FILE__)
 ActionController::Base.helper Widgets::ShowhideHelper
 
+
 ##### Tooltip #####
-require widgets_path + 'tooltip_helper'
+require File.expand_path(widgets_path + 'tooltip_helper', __FILE__)
 ActionController::Base.helper Widgets::TooltipHelper
 
+
 ##### Progressbar #####
-require widgets_path + 'progressbar_helper'
+require File.expand_path(widgets_path + 'progressbar_helper', __FILE__)
 ActionController::Base.helper Widgets::ProgressbarHelper
 
 ##### Spiffy Corners #####
-require widgets_path + 'spiffy_corners/spiffy_corners_helper'
+require File.expand_path(widgets_path + 'spiffy_corners/spiffy_corners_helper', __FILE__)
 ActionController::Base.helper Widgets::SpiffyCorners::SpiffyCornersHelper
 
+
 ##### UtilsHelper #####
-require widgets_path + 'utils_helper'
+require File.expand_path(widgets_path + 'utils_helper', __FILE__)
 ActionController::Base.helper Widgets::UtilsHelper
 
