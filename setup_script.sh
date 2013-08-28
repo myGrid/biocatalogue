ae9264f4063491396b515dbbe695773cead74600
anenadic <anenadic@cs.man.ac.uk>
2013-08-28 11:50:24 +0100

    Modified the setup script to copy sunspot Solr config file too.

--- setup_script.sh
+++ setup_script.sh
@@ -24,7 +24,7 @@ bundle install
 
 [ ! -f ${APP_DIR}/config/database.yml ] && cp ${APP_DIR}/config/database.yml.pre ${APP_DIR}/config/database.yml && echo "Created ${APP_DIR}/config/database.yml"
 [ ! -f ${APP_DIR}/config/memcache.yml ] && cp ${APP_DIR}/config/memcache.yml.pre ${APP_DIR}/config/memcache.yml  && echo "Created ${APP_DIR}/config/memcache.yml"
-[ ! -f ${APP_DIR}/config/solr.yml ] && cp ${APP_DIR}/config/solr.yml.pre ${APP_DIR}/config/solr.yml  && echo "Created ${APP_DIR}/config/solr.yml"
+[ ! -f ${APP_DIR}/config/sunspot.yml ] && cp ${APP_DIR}/config/sunspot.yml.pre ${APP_DIR}/config/sunspot.yml  && echo "Created ${APP_DIR}/config/sunspot.yml"
 [ ! -f ${APP_DIR}/config/initializers/biocat_local.rb ] && cp ${APP_DIR}/config/initializers/biocat_local.rb.pre ${APP_DIR}/config/initializers/biocat_local.rb && echo "Created ${APP_DIR}/config/initializers/biocat_local.rb"
 [ ! -f ${APP_DIR}/config/initializers/mail.rb ] && cp ${APP_DIR}/config/initializers/mail.rb.pre ${APP_DIR}/config/initializers/mail.rb && echo "Created ${APP_DIR}/config/initializers/mail.rb"
 [ ! -f ${APP_DIR}/config/initializers/secret_token.rb ] && cp ${APP_DIR}/config/initializers/secret_token.rb.pre ${APP_DIR}/config/initializers/secret_token.rb && echo "Created ${APP_DIR}/config/initializers/secret_token.rb"
@@ -35,7 +35,7 @@ echo
 echo "Please remember to configure the following to suit your environment:" 
 echo "config/database.yml"
 echo "config/memcache.yml"
-echo "config/solr.yml"
+echo "config/sunspot.yml"
 echo "config/initializers/biocat_local.rb"
 echo "config/initializers/mail.rb"
 echo "config/initializers/secret_token.rb"
