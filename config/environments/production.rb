23b15bf6aba5143b537b4145f83b5132d4a8d3bc
Aleksandra Nenadic <anenadic@cs.man.ac.uk>
2013-12-18 16:01:05 +0000

    Moved log rotation and cronolog parameters to biocat_local.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,22 +5,6 @@ BioCatalogue::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
-  # Log rotation
-  # Set this to true to rotate the logs (recommended for production)
-  ROTATE_LOGS = false
-
-  # Use cronolog for log rotation in production
-  # Example CRONOLOG_PARAMS = "/my/server/cronolog /my/application/file/log/production.log.%Y%m%d"
-  CRONOLOG_PARAMS = nil
-
-  # Rotate logs when they reach a size of 10M and keep no more than 10 of these
-  #config.logger = Logger.new(config.log_path, 10, (1024**2)*10)
-
-  #if ROTATE_LOGS && CRONOLOG_PARAMS
-  #  config.logger = Logger.new(IO.popen(CRONOLOG_PARAMS, "w"))
-  #  config.logger.level = Logger::INFO
-  #end
-
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
