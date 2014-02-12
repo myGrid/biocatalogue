8103477a0e0e1b8c01e3a8135dea4b62d21cd37c
Aleksandra Nenadic <anenadic@cs.man.ac.uk>
2014-02-12 10:38:34 +0000

    Increased the max running time for delayed job.

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -3,7 +3,7 @@ BioCatalogue::Application.configure do
   Delayed::Worker.destroy_failed_jobs = false
   Delayed::Worker.sleep_delay = 3
   Delayed::Worker.max_attempts = 1
-  Delayed::Worker.max_run_time = 1.hour
+  Delayed::Worker.max_run_time = 6.hour
   Delayed::Worker.backend = :active_record
 
   #Delayed::Worker.logger = Delayed::Worker.logger = ActiveSupport::BufferedLogger.new(Rails.root.join('log/worker.log'),Logger::INFO)
