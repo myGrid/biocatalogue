a97c776934d8eedf39718a08e641b0d887adc0eb
Niall Beard <niallbeard@gmail.com>
2014-04-22 17:18:47 +0100

    Check whether record from import is different from that in the database and display those that are

--- app/controllers/external_curation_controller.rb
+++ app/controllers/external_curation_controller.rb
@@ -1,11 +1,15 @@
 class ExternalCurationController < ApplicationController
+
   include CurationHelper
+
+
   def export
     services = xls_of_services
     rest_methods = xls_of_rest_methods
     soap_operations = xls_of_soap_operations
     respond_to do |format|
-      format.xls { render :locals => {:services => services,
+      format.xls { render :locals => {:headers => headers,
+                                      :services => services,
                                       :rest_methods => rest_methods,
                                       :soap_operations => soap_operations}
       }
@@ -20,39 +24,74 @@ class ExternalCurationController < ApplicationController
 
   def import
     file = params[:xls_file]
-
     begin
       spreadsheet = open_spreadsheet(file)
-    rescue e
-      flash[:notice] = "There was an error loading the document. Please read the instructions carefully."
+      raise "There was a problem loading the spreadsheet. Please ensure you have read the instructions" if spreadsheet.nil?
+      modified_lines = find_modified_rows(spreadsheet)
+      respond_to do |format|
+        format.html {render :locals => {:xls => modified_lines}}
+      end
+    rescue Exception => e
+      flash[:notice] = "There was an error loading the document"
+      @error = "#{e.backtrace.join("\n")}"
       respond_to do |format|
         format.html {render :new_import}
       end
     end
-    modified_lines = find_modified_rows(spreadsheet)
-    respond_to do |format|
-      format.html {render :locals => {:xls => modified_lines}}
-    end
   end
 
-
-
-
+  private
   def find_modified_rows spreadsheet
-
-    spreadsheet.each_with_pagename do |name, sheet|
-      header = sheet.row(1)
-        (2..sheet.last_row).each do |i|
-          row = Hash[[header, sheet.row(i)].transpose]
-          name.constantize.find_by_unique_code(row["Service ID"])
-          service = Service.find_by_unique_code(row["Service ID"])
-
+    modified_rows = {}
+    unless spreadsheet.nil?
+      spreadsheet.each_with_pagename do |sheet_name, sheet|
+        header = sheet.row(1)
+        if check_headers_match(header, sheet_name)
+          modified_rows[sheet_name] = []
+          (2..sheet.last_row).each do |i|
+            service_row = sheet.row(i)
+            service_hash = Hash[[header, service_row].transpose]
+            modified_rows[sheet_name] << service_hash if record_changed?(sheet_name, service_row)
+          end
+        else
+          raise 'At least one of the column headings in the imported document do not match up to the original export headers'
         end
+      end
     end
-
-    return modified_lines
+    return modified_rows
   end
 
+  def record_changed?(record_type, record)
+    record_class = record_type.singularize.constantize
+    db_record = record_class.find(record.first)
+    db_record_as_csv = db_record.as_csv
+    return record != db_record_as_csv
+  end
+
+  # Check the column titles match up with the original ones
+  def check_headers_match new_header, sheet_name
+    return new_header == get_original_header(sheet_name)
+  end
+
+  def get_original_header sheet_name
+    case sheet_name
+      when 'RestMethods'
+        return REST_METHOD_COLUMNS
+      when 'SoapOperations'
+        return SOAP_OPERATION_COLUMNS
+      when 'Services'
+        return SERVICE_COLUMNS
+      else
+        raise 'Could not match headengs to new headings'
+    end
+  end
+
+
+  def headers
+    return {:service_columns => SERVICE_COLUMNS,
+            :soap_operation_columns => SOAP_OPERATION_COLUMNS,
+            :rest_method_columns => REST_METHOD_COLUMNS}
+  end
 
   def open_spreadsheet(file)
     case File.extname(file.original_filename)
